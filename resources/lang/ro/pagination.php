53182bb2ced9be009edfead5856c473013d5853e
ideacatlab <razvan@ideacat.ro>
2021-01-10 22:43:12 +0200

    or global with merge

--- resources/lang/ro/pagination.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-return [
-    'previous' => '« Anterioara',
-    'next' => 'Următoarea »',
-];
--- resources/lang/ro/passwords.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-return [
-    'password' => 'Parolele trebuie să aibă cel puțin opt caractere și să corespundă confirmării.',
-    'reset' => 'Parola ta a fost resetata!',
-    'sent' => 'V-am trimis prin e-mail linkul de resetare a parolei!',
-    'token' => 'Acest token de resetare a parolei este nevalid.',
-    'user' => 'Nu putem găsi un utilizator cu acea adresă de e-mail.',
-];
